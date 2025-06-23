4eca202b5de0922c1870850db8d3076bf36dd287
m560560 <99889286+m560560@users.noreply.github.com>
2025-06-23 10:35:28 +0300

    Create src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+{
+  "name": "FootballTeamSplitter",
+  "version": "1.0.0",
+  "dependencies": {
+    "react": "18.2.0",
+    "react-native": "0.74.5"
+  },
+  "scripts": {
+    "start": "react-native start",
+    "android": "react-native run-android",
+    "ios": "react-native run-ios"
+  }
+}
